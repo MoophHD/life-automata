4950c2fc28aca1f66d8986d246a331b578da9ec9
moophhd <d_sizykh@mail.ru>
2020-08-19 21:22:10 +0300

    basic scale for input-range

--- client/src/components/RangeInput.js
+++ client/src/components/RangeInput.js
@@ -3,25 +3,62 @@ import styled from "styled-components";
 
 const RangeInput = ({ style, min, max, step, defaultValue, register }) => {
   return (
-    <Input
-      name="interval"
-      style={style}
-      type="range"
-      min={min}
-      max={max}
-      step={step}
-      defaultValue={defaultValue}
-      ref={register}
-    />
+    <>
+      {/* <Ruler length={~~((max-min)/step)} /> */}
+      <Ruler length={5} />
+
+      <Input
+        name="interval"
+        style={style}
+        type="range"
+        min={min}
+        max={max}
+        step={step}
+        defaultValue={defaultValue}
+        ref={register}
+      />
+    </>
   );
 };
 //stump = {0: {title: ""}}
-const Ruler = ({stumps={}, length, }) => {
+const Ruler = ({ stumps = {}, length }) => {
   //must return lines like in ruler with stumps representing bigger lines with titles udnerneath
-  return <></>
-}
+  return <Scale length={length} />;
+};
 
-const thumpSize = .9; //rem
+const color = "tomato";
+const width = 2; //px
+
+const gedGradientRecepy = (length) => {
+  const recepy = [];
+  const gapPercentage = 100 / (length - 1);
+
+  //draws line than gap
+  for (let i = 0; i < length - 1; i++) {
+    recepy.push(`${color} ${gapPercentage * i}%`);
+    recepy.push(`${color} calc(${gapPercentage * i}% + ${width}px)`);
+    recepy.push(`transparent calc(${gapPercentage * i}% + ${width}px)`);
+    recepy.push(`transparent ${gapPercentage * (i + 1)}%`);
+  }
+
+  //last gap has to be shorter in order to accomodate the last line
+  recepy.pop();
+  recepy.push(`transparent calc(100% - ${width}px)`);
+  // *.5 is a workaround, google renders the list line at 2 * width
+  recepy.push(`${color} calc(100% - ${width * 0.5}px), ${color} 100%`);
+
+  return recepy.join(",");
+};
+const Scale = styled.div`
+  width: 200px;
+  height: 40px;
+  background: linear-gradient(
+    90deg,
+    ${(props) => gedGradientRecepy(props.length)}
+  );
+`;
+
+const thumpSize = 0.9; //rem
 const borderSize = 0.3; //rem
 
 const Input = styled.input`

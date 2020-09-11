7935afc1b837e4f77f98c76f79d743891116fa44
moophhd <d_sizykh@mail.ru>
2020-09-11 15:20:26 +0300

    testing env variables set in heroku

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ const path = require('path');
 const app = express();
 app.set("trust proxy", 1);
 
+
+console.log(`git id ${process.env.GITHUB_CLIENT_ID}`);
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 app.use(cookieSession({ maxAge: 24 * 60 * 60 * 1000, keys: ["secret"] }));
@@ -35,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT;
 async function start() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,

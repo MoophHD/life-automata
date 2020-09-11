29a0eb7efd956169c81549ba703f4420be7655c9
moophhd <d_sizykh@mail.ru>
2020-09-11 16:10:14 +0300

    move user variable to outer scope in passport strategy

--- server/passport/setup.js
+++ server/passport/setup.js
@@ -24,18 +24,21 @@ passport.use(
       scope: ["user:email"],
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log(`github passport callback`);
-      console.log(profile);
       try {
         const email = profile.emails[0].value;
         const username = profile.username;
+        let user;
 
         const candidate = await User.findOne({ email, provider: "Github" });
         if (!candidate) {
-          const user = new User({ username, email, provider: "Github" });
+          user = new User({ username, email, provider: "Github" });
 
           await user.save();
         }
+        console.log(`user`)
+        console.log(user);
+        console.log(`candidate`);
+        console.log(candidate);
         done(null, candidate || user);
       } catch (e) {
         console.log(`An error has occured in password.js middleware, ${e}`);
@@ -59,9 +62,10 @@ passport.use(
         const username = profile.displayName;
 
         const candidate = await User.findOne({ email, provider: "Google" });
+        let user;
 
         if (!candidate) {
-          const user = new User({ username, email, provider: "Google" });
+          user = new User({ username, email, provider: "Google" });
           await user.save();
         }
         done(null, candidate || user);
